46cd08f8e94c2f8bf57acce33209dce0f242ff56
Jessica Hsiao <136421644+jh1231223@users.noreply.github.com>
2025-04-04 02:19:01 -0500

    Remove all files except doc folder

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomepageComponent } from './homepage/homepage.component';
-import { MovieDetailsComponent } from './movie-details/movie-details.component';
-
-export const routes: Routes = [
-    {path: '', component: HomepageComponent},
-    {path: 'movies/:id', component: MovieDetailsComponent},
-];
