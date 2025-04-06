e017ac46032300596fe98dd57df424f6190d32f0
Sophie Huang <71316753+chihyu0917@users.noreply.github.com>
2025-04-05 21:39:24 -0500

    filter with years

--- CineTravel/Frontend/src/app/homepage/homepage.component.ts
+++ CineTravel/Frontend/src/app/homepage/homepage.component.ts
@@ -15,6 +15,15 @@ import { Movie } from '../movie';
 })
 export class HomepageComponent implements OnInit {
   title = 'Welcome to the CineTravel!';
+  searchTerm: string = "";
+  selectedGenre: string = '';
+  selectedYear: string = '';
+  selectedLocation: string = '';
+
+  // 如果你有預先設定可篩選選項，則可加入以下陣列
+  genres: string[] = [];
+  years: string[] = [];
+  locations: string[] = [];
   // 存放從後端取得的所有電影
   filteredMovies: Movie[] = [];
 
@@ -25,10 +34,37 @@ export class HomepageComponent implements OnInit {
     this.movieService.getAllMovies().subscribe({
       next: (movies: Movie[]) => {
         this.filteredMovies = movies;
+        // 從電影資料中提取所有年份
+        this.years = Array.from(new Set(movies.map(movie => movie.year.toString())));
       },
       error: (err) => {
         console.error('Failed to load movies', err);
       }
     });
+  }
+
+  loadMovies(): void {
+    this.movieService.filterMovies({
+      search: this.searchTerm,
+      genre: this.selectedGenre,
+      year: this.selectedYear,
+      location: this.selectedLocation
+    }).subscribe({
+      next: (movies: Movie[]) => {
+        this.filteredMovies = movies;
+        // 如果需要，你也可以根據返回結果動態產生篩選選項
+      },
+      error: (err) => {
+        console.error('搜尋電影時發生錯誤:', err);
+      }
+    });
+  }
+
+  onSearch(): void {
+    this.loadMovies();
+  }
+
+  onFilterChange(): void {
+    this.loadMovies();
   }
 }
