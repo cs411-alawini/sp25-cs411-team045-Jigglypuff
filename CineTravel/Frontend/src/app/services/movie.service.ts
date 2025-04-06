3805574f6946f5fc9d9d4aea5de63f12156d0721
Sophie Huang <71316753+chihyu0917@users.noreply.github.com>
2025-04-05 21:46:14 -0500

    filter with years

--- CineTravel/Frontend/src/app/services/movie.service.ts
+++ CineTravel/Frontend/src/app/services/movie.service.ts
@@ -1,6 +1,6 @@
 // src/app/services/movie.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Movie } from '../movie';
 
@@ -14,8 +14,21 @@ export class MovieService {
   constructor(private http: HttpClient) {}
 
   // 若有需要搜尋功能可保留
-  searchMovies(term: string): Observable<Movie[]> {
-    return this.http.get<Movie[]>(`${this.apiUrl}?search=${term}`);
+  filterMovies(filters: { search?: string; genre?: string; year?: string; location?: string }): Observable<Movie[]> {
+    let params = new HttpParams();
+    if (filters.search) {
+      params = params.set('search', filters.search);
+    }
+    if (filters.genre) {
+      params = params.set('genre', filters.genre);
+    }
+    if (filters.year) {
+      params = params.set('year', filters.year);
+    }
+    if (filters.location) {
+      params = params.set('location', filters.location);
+    }
+    return this.http.get<Movie[]>(this.apiUrl, { params });
   }
 
   // 新增：取得所有電影
