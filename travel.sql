8e4aeac9115905b4a9338f21604d2ab5e99dd45a
Sophie Huang <71316753+chihyu0917@users.noreply.github.com>
2025-04-28 17:02:50 -0500

    db

--- travel.sql
+++ travel.sql
@@ -455,6 +455,45 @@ INSERT INTO `movie_new` VALUES (13755,'Glass Accept Lawyer_12754',5.9,109,2004,'
 UNLOCK TABLES;
 
 --
+-- Dumping routines for database 'travel'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `GetFlightsBeforeAvg` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetFlightsBeforeAvg`(
+    IN p_origin VARCHAR(10)
+)
+BEGIN
+    SELECT 
+        f1.*
+    FROM 
+        flight f1
+    WHERE 
+        f1.origin = p_origin
+        AND STR_TO_DATE(f1.sched_dep_time, '%H:%i')
+            < (
+                SELECT AVG(STR_TO_DATE(f2.sched_dep_time, '%H:%i'))
+                FROM flight f2
+                WHERE f2.origin = p_origin
+            )
+    ORDER BY 
+        STR_TO_DATE(f1.sched_dep_time, '%H:%i');
+    
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
 -- Final view structure for view `hotel_country_view`
 --
 
@@ -481,4 +520,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-28 16:34:36
+-- Dump completed on 2025-04-28 17:01:24
